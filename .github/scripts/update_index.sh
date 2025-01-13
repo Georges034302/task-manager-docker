7222beec013d04fe3d0c18e49f769d26d74b3f9a
Georges <georges.boughantous@gmail.com>
2025-01-13 09:51:24 +0000

    update HTML

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -16,22 +16,13 @@ UNIT_TEST_RESULTS=$(cat "$2")
 cp "$HTML_FILE" "$HTML_FILE.bak"
 
 # Replace the Pending Tasks section with ToDo tasks (do not remove the section)
-sed -i "/<ul id=\"pending\">/,/<\/ul>/c\\
-<ul id=\"pending\">\\
-$TODO_TASKS\\
-</ul>" "$HTML_FILE"
+sed -i "s|<ul id=\"pending\">.*</ul>|<ul id=\"pending\">$TODO_TASKS</ul>|" "$HTML_FILE"
 
 # Replace the Completed Tasks section with Done tasks (do not remove the section)
-sed -i "/<ul id=\"completed\">/,/<\/ul>/c\\
-<ul id=\"completed\">\\
-$DONE_TASKS\\
-</ul>" "$HTML_FILE"
+sed -i "s|<ul id=\"completed\">.*</ul>|<ul id=\"completed\">$DONE_TASKS</ul>|" "$HTML_FILE"
 
 # Replace the Unit Test Results section with the actual test results
-sed -i "/<pre id=\"unittest\">/,/<\/pre>/c\\
-<pre id=\"unittest\">\\
-$UNIT_TEST_RESULTS\\
-</pre>" "$HTML_FILE"
+sed -i "s|<pre id=\"unittest\">.*</pre>|<pre id=\"unittest\">$UNIT_TEST_RESULTS</pre>|" "$HTML_FILE"
 
 # Configure Git and push changes
 git config --global user.name "github-actions"
