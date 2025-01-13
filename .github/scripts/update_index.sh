ed4bacc0a6889c6a5227bc520f4259643f5524e3
Georges <georges.boughantous@gmail.com>
2025-01-13 10:06:38 +0000

    update HTML

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -4,21 +4,13 @@
 HTML_FILE="index.html"
 
 # Read the ToDo tasks from $1 (pending tasks)
-TODO_TASKS=$(grep -A 1000 "ToDo Tasks:" "$1" | grep -B 1000 "Done Tasks:" | sed '1d;$d' | sed 's/^/<li>/;s/$/<\/li>/')
+TODO_TASKS=$(grep -A 1000 "ToDo Tasks:" "$1" | grep -B 1000 "Done Tasks:" | sed '1d;$d')
 
 # Read the Done tasks from $1 (completed tasks)
-DONE_TASKS=$(grep -A 1000 "Done Tasks:" "$1" | sed '1d' | sed 's/^/<li>/;s/$/<\/li>/')
+DONE_TASKS=$(grep -A 1000 "Done Tasks:" "$1" | sed '1d')
 
 # Read the Unit Test results from $2
 UNIT_TEST_RESULTS=$(cat "$2")
-
-# Escape any special characters for use in sed (e.g., & and /)
-TODO_TASKS=$(echo "$TODO_TASKS" | sed 's/[&/\]/\\&/g')
-DONE_TASKS=$(echo "$DONE_TASKS" | sed 's/[&/\]/\\&/g')
-UNIT_TEST_RESULTS=$(echo "$UNIT_TEST_RESULTS" | sed 's/[&/\]/\\&/g')
-
-# Backup the original HTML file to prevent data loss
-cp "$HTML_FILE" "$HTML_FILE.bak"
 
 # Replace the Pending Tasks section with ToDo tasks
 sed -i "/<ul id=\"pending\">/,/<\/ul>/c\\
