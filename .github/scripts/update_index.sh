4ec46354a3816583ef33e029e074206da020763e
Georges <georges.boughantous@gmail.com>
2025-01-13 09:37:35 +0000

    update HTML

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -9,15 +9,21 @@ TODO_TASKS=$(grep -A 1000 "ToDo Tasks:" "$1" | grep -B 1000 "Done Tasks:" | sed 
 # Read the Unit Test results from $2
 UNIT_TEST_RESULTS=$(cat "$2")
 
+# Escape special characters in the tasks and unit test results for sed
+TODO_TASKS_ESCAPED=$(printf '%s' "$TODO_TASKS" | sed 's/[&/\]/\\&/g')
+UNIT_TEST_RESULTS_ESCAPED=$(printf '%s' "$UNIT_TEST_RESULTS" | sed 's/[&/\]/\\&/g')
+
 # Replace the Pending Tasks section with ToDo tasks
-sed -i 's#<ul id="pending">.*</ul>#<ul id="pending">\
-'"$TODO_TASKS"'\
-</ul>#' $HTML_FILE
+sed -i "/<ul id=\"pending\">/,/<\/ul>/c\\
+<ul id=\"pending\">\\
+$TODO_TASKS_ESCAPED\\
+</ul>" $HTML_FILE
 
 # Replace the Unit Test Results section with the actual test results
-sed -i 's#<pre id="unittest">.*</pre>#<pre id="unittest">\
-'"$UNIT_TEST_RESULTS"'\
-</pre>#' $HTML_FILE
+sed -i "/<pre id=\"unittest\">/,/<\/pre>/c\\
+<pre id=\"unittest\">\\
+$UNIT_TEST_RESULTS_ESCAPED\\
+</pre>" $HTML_FILE
 
 # Configure Git and push changes
 git config --global user.name "github-actions"
