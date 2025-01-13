dadca1b84372d145ec5243e11fe1eff58f48d855
Georges <georges.boughantous@gmail.com>
2025-01-13 09:41:52 +0000

    update HTML

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -6,24 +6,47 @@ HTML_FILE="index.html"
 # Read the ToDo tasks from $1 (pending tasks)
 TODO_TASKS=$(grep -A 1000 "ToDo Tasks:" "$1" | grep -B 1000 "Done Tasks:" | sed '1d;$d' | sed 's/^/<li>/;s/$/<\/li>/')
 
+# Read the Done tasks from $1 (completed tasks)
+DONE_TASKS=$(grep -A 1000 "Done Tasks:" "$1" | sed '1d' | sed 's/^/<li>/;s/$/<\/li>/')
+
 # Read the Unit Test results from $2
 UNIT_TEST_RESULTS=$(cat "$2")
 
-# Escape special characters in the tasks and unit test results for sed
-TODO_TASKS_ESCAPED=$(printf '%s' "$TODO_TASKS" | sed 's/[&/\]/\\&/g')
-UNIT_TEST_RESULTS_ESCAPED=$(printf '%s' "$UNIT_TEST_RESULTS" | sed 's/[&/\]/\\&/g')
+# Backup the original HTML file to prevent data loss
+cp "$HTML_FILE" "$HTML_FILE.bak"
 
 # Replace the Pending Tasks section with ToDo tasks
-sed -i "/<ul id=\"pending\">/,/<\/ul>/c\\
-<ul id=\"pending\">\\
-$TODO_TASKS_ESCAPED\\
-</ul>" $HTML_FILE
+awk -v tasks="$TODO_TASKS" '
+  /<ul id="pending">/ { 
+    print "<ul id=\"pending\">"; 
+    print tasks; 
+    next;
+  }
+  /<\/ul>/ { print "</ul>"; next }
+  { print }
+' "$HTML_FILE.bak" > "$HTML_FILE"
+
+# Replace the Completed Tasks section with Done tasks
+awk -v tasks="$DONE_TASKS" '
+  /<ul id="completed">/ { 
+    print "<ul id=\"completed\">"; 
+    print tasks; 
+    next;
+  }
+  /<\/ul>/ { print "</ul>"; next }
+  { print }
+' "$HTML_FILE" > "$HTML_FILE.tmp" && mv "$HTML_FILE.tmp" "$HTML_FILE"
 
 # Replace the Unit Test Results section with the actual test results
-sed -i "/<pre id=\"unittest\">/,/<\/pre>/c\\
-<pre id=\"unittest\">\\
-$UNIT_TEST_RESULTS_ESCAPED\\
-</pre>" $HTML_FILE
+awk -v results="$UNIT_TEST_RESULTS" '
+  /<pre id="unittest">/ { 
+    print "<pre id=\"unittest\">"; 
+    print results; 
+    next;
+  }
+  /<\/pre>/ { print "</pre>"; next }
+  { print }
+' "$HTML_FILE" > "$HTML_FILE.tmp" && mv "$HTML_FILE.tmp" "$HTML_FILE"
 
 # Configure Git and push changes
 git config --global user.name "github-actions"
